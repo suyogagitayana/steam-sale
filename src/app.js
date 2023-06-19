35c9ead66a2e6d1da2a4d4b2bfd6902b916b1da2
Suyoga Gitayana <suyogagitayana@gmail.com>
2023-06-19 21:38:01 +0800

    fix: data loading and responsive

--- src/app.js
+++ src/app.js
@@ -48,7 +48,7 @@ const App = () => {
     };
 
     return <div className=' text-gray-200'>
-        <div className='text-center text-xs p-2 bg-yellow cl-yellow fixed inset-x-0 top-0 left-0'>This project is just for fun. I didnt guarantee the accuracy of any information here. All data is provided by <a className='cl-blue' href='https://apidocs.cheapshark.com/' target='_blank' rel='noreferrer'>CheapShark API</a></div>
+        <div className='text-center text-xs p-2 bg-yellow cl-yellow inset-x-0 top-0 left-0'>This project is just for fun. All data is provided by <a className='cl-blue' href='https://apidocs.cheapshark.com/' target='_blank' rel='noreferrer'>CheapShark API</a></div>
         <div className='flex flex-wrap content-center items-center mt-8'>
             <h1 className='flex justify-center font-bold text-5xl w-full gap-2 mb-1'>
                 STEAM<span className='font-light'>SALE</span>
@@ -59,18 +59,18 @@ const App = () => {
                 placeholder={'Type to search games...'}
                 onSearch={setSearch} />
             <div className='flex w-full items-center gap-4 my-4'>
-                <p className='w-1/3'>Price Ranges</p>
+                <p className='w-1/3 text-sm'>Price Ranges</p>
                 <input
                     type='number'
                     value={lowerPrice}
                     className='bg-dark border-gray-300 rounded-sm border-solid border-1 px-2 h-8 text-xs w-1/3'
-                    placeholder='Minimum Price ($)'
+                    placeholder='Minimum($)'
                     onChange={e => setLowerPrice(!isEmpty(e.target.value) ? e.target.value : undefined)} />
                 <input
                     type='number'
                     value={upperPrice}
                     className='bg-dark border-gray-300 rounded-sm border-solid border-1 px-2 h-8 text-xs w-1/3'
-                    placeholder='Maximum Price ($)'
+                    placeholder='Maximum($)'
                     onChange={e => setUpperPrice(!isEmpty(e.target.value) ? e.target.value : undefined)} />
             </div>
         </div>
@@ -91,11 +91,11 @@ const App = () => {
 
             return <div key={steamAppID} className='bg-dark flex p-2 gap-2 mb-2 border-1 border-gray-600 rounded-sm relative'>
                 <div className='flex items-center w-1/5'>
-                    <a className='w-full' href={`https://store.steampowered.com/app/${steamAppID}`}>
-                        <img className='w-full' src={thumb} />
+                    <a className='w-full bg-gray-500' href={`https://store.steampowered.com/app/${steamAppID}`} style={{height: '50.25px'}}>
+                        <img className='h-full' src={thumb} style={{objectFit: 'cover'}}/>
                     </a>
                 </div>
-                <div className='w-3/5'>
+                <div className='w-1/2 sm:w-3/5'>
                     <h3 className='font-medium text-sm break-words'>
                         <a href={`https://store.steampowered.com/app/${steamAppID}`}>{title}</a>
                     </h3>
@@ -103,11 +103,11 @@ const App = () => {
                 </div>
                 <div className='flex items-center justify-end w-1/5'>
                     {isOnSale === '1' && <div className='flex items-center justify-center bg-green cl-green w-1/2 py-1'>
-                        <p className='font-medium text-sm'>-{Math.round(savings)}%</p>
+                        <p className='font-medium text-xs'>-{Math.round(savings)}%</p>
                     </div>}
                     <div className='flex flex-wrap items-center w-1/2 pl-3'>
                         <p className='w-full font-light text-gray-400 text-xs'>{isOnSale === '1' && <strike>${normalPrice}</strike>}</p>
-                        <p className='w-full font-normal text-base'>${salePrice}</p>
+                        <p className='w-full font-normal text-sm'>${salePrice}</p>
                     </div>
                 </div>
             </div>;
